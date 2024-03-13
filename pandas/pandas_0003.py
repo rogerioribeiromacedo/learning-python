24d029890211c122431a1b285ef9f34a0880d828
Rogério Ribeiro Macêdo <115092500+rogerioribeiromacedo@users.noreply.github.com>
2024-03-13 11:28:11 -0300

    Update scripts

--- pandas/pandas_0003.py
+++ pandas/pandas_0003.py
@@ -1,18 +1,27 @@
 import pandas as pd
 
-# Importando dados de arquivo csv (tabela_periodica.csv)
-# Origem dos dados: https://pubchem.ncbi.nlm.nih.gov/periodic-table/
-df_geral = pd.read_csv("tabela_periodica.csv", sep=",")
-
-#
-# Criando um dataframe formado apenas por algumas colunas dos dados originais
-df_colunas = df_geral[["AtomicNumber", "Symbol", "Name", "AtomicMass", "GroupBlock"]]
+df = pd.read_excel("tabela_periodica.xlsx")
 
 # Valores máximo e mínimo da coluna 'AtomicMass'
-atomic_mass_max = df_colunas["AtomicMass"].max()
-atomic_mass_min = df_colunas["AtomicMass"].min()
+atomic_mass_max = df["AtomicMass"].max()
+atomic_mass_min = df["AtomicMass"].min()
+
+# Raio atômico
+atomic_radius_max = df["AtomicRadius"].max()
+atomic_radius_min = df["AtomicRadius"].min() 
+
+# Criando um 'set' com os elementos da coluna 'StandardState'
+standard_state = set()
+for i in list(df['StandardState']):
+    standard_state.add(i)
 
 print("#\n# Resultados\n#")
-print(f"Número de elementos..........: {len(df_colunas)}")
-print(f"Maior valor de massa atômica.: {atomic_mass_max}")
-print(f"Menor valor de massa atômica.: {atomic_mass_min}")
+print(f"# - Número de elementos..........: {len(df)}")
+print("# - Massa atômica:")
+print(f"    + Maior valor de massa atômica.: {atomic_mass_max}")
+print(f"    + Menor valor de massa atômica.: {atomic_mass_min}")
+print("# - Raio atômico:")
+print(f"    + Maior raio atômico...........: {atomic_radius_max}")
+print(f"    + Menor raio atômico...........: {atomic_radius_min}")
+print("# - Estados padrão:")
+print(f"    + {standard_state}")
