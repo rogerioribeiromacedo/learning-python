eb06c85bb26efd893855e52826da49ee71baca21
Rogério Ribeiro Macêdo <115092500+rogerioribeiromacedo@users.noreply.github.com>
2024-04-04 11:02:09 -0300

    Update

--- pandas/.ipynb_checkpoints/pandas_0001-checkpoint.py
+++ pandas/.ipynb_checkpoints/pandas_0001-checkpoint.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
 
-# Criando dataframe com alguns elementos da tabela periodica
+# Criando dataframe com alguns elementos da tabela periódica
 df = pd.DataFrame(
     {"Simbolo": ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne"], 
-     "Numero atomico": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+     "Numero atômico": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
      "Nome elemento": ["Hidrogênio", "Hélio", "Lítio", "Berílio", "Boro", "Carbono", "Nitrogênio", "Oxigênio", "Flúor", "Neônio"]}
 )
 
